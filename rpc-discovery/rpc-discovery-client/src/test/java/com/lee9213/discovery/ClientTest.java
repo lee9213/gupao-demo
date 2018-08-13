a96a9c2177b91ef358060d9d012dd33302645bcc
lee9213 <lee9213@163.com>
2018-08-13 14:36:02 +0800

    modify discovery client

--- rpc-discovery/rpc-discovery-client/src/test/java/com/lee9213/discovery/ClientTest.java
+++ rpc-discovery/rpc-discovery-client/src/test/java/com/lee9213/discovery/ClientTest.java
@@ -2,6 +2,7 @@ package com.lee9213.discovery;
 
 import com.lee9213.discovery.client.proxy.RpcProxy;
 import com.lee9213.service.IHelloService;
+import org.junit.Test;
 
 /**
  * @author lee9213@163.com
@@ -11,9 +12,10 @@ import com.lee9213.service.IHelloService;
 public class ClientTest {
 
 
-    public static void main(String[] args) {
-        for(int i=0;i<20;i++){
-            IHelloService helloService = RpcProxy.newInstance(IHelloService.class, i%2+1 + ".0");
+    @Test
+    public void test() {
+        for (int i = 0; i < 20; i++) {
+            IHelloService helloService = RpcProxy.newInstance(IHelloService.class, i % 2 + 1 + ".0");
             String message = helloService.hello("浪里小白龙");
             System.out.println(message);
         }
