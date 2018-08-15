8c1baafbe555f75076da546a10554012583a7734
lee9213 <lee9213@163.com>
2018-08-15 22:51:53 +0800

    add dubbo server api demo

---
+++ dubbo-demo/dubbo-server-api/src/main/java/com/lee9213/service/IUserService.java
@@ -0,0 +1,12 @@
+package com.lee9213.service;
+
+/**
+ * @author lee9213@163.com
+ * @version 1.0
+ * @date 2018-08-14 20:39
+ */
+public interface IUserService {
+
+
+    String hello(String message);
+}
